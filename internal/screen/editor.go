c83df79cd8aba9bfbee5987730334b07c656341b
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-04-20 22:27:23 +0530

    Modes - base implementation

--- internal/screen/editor.go
+++ internal/screen/editor.go
@@ -8,15 +8,24 @@ import (
 	"github.com/manojVivek/vim_go/internal/actions"
 )
 
+type Vertex struct {
+	X, Y int
+}
+
+type mode string
+
+const (
+	MODE_NORMAL       mode = "NORMAL"
+	MODE_INSERT       mode = "INSERT"
+	MODE_COMMAND_LINE mode = "COMMAND_LINE"
+)
+
+var currentMode = MODE_NORMAL
 var dataBuffer []string = make([]string, 1)
 var textFrame [][]rune
 var startLine int = 0
 var cursorPosBuffer Vertex
 var cursorPosScreen Vertex
-
-type Vertex struct {
-	X, Y int
-}
 
 // InitBuffer - Intialize the textFrame with filecontent / blank file content
 func InitBuffer() {
@@ -38,78 +47,97 @@ func InitBuffer() {
 
 // HandleUserActions - function that listens the user input channel and handles it appropriately
 func HandleUserActions(c chan actions.Event) {
-	escapePressed := false
 	for e := range c {
 		if e.Kind != "KEY_PRESS" {
 			continue
 		}
-		switch e.Value {
-		case tcell.KeyEscape:
-			escapePressed = true
-		case tcell.KeyEnter:
-			newBuffer := make([]string, len(dataBuffer)+1)
-			copy(newBuffer, dataBuffer)
-			if cursorPosBuffer.Y < len(dataBuffer)-1 {
-				for i := len(newBuffer) - 1; i > cursorPosBuffer.Y; i-- {
-					newBuffer[i] = newBuffer[i-1]
-				}
-			}
-
-			if cursorPosBuffer.X < len(dataBuffer[cursorPosBuffer.Y]) {
-				newBuffer[cursorPosBuffer.Y] = dataBuffer[cursorPosBuffer.Y][:cursorPosBuffer.X]
-				newBuffer[cursorPosBuffer.Y+1] = dataBuffer[cursorPosBuffer.Y][cursorPosBuffer.X:]
-			}
-			cursorPosBuffer.Y++
-			cursorPosBuffer.X = 0
-			dataBuffer = newBuffer
-			syncTextFrame()
-		case tcell.KeyLeft:
-			if cursorPosBuffer.X != 0 {
-				cursorPosBuffer.X--
-				syncCursor()
-			}
-		case tcell.KeyRight:
-			if cursorPosBuffer.X != len(dataBuffer[cursorPosBuffer.Y]) {
-				cursorPosBuffer.X++
-				syncCursor()
-			}
-		case tcell.KeyDown:
-			if cursorPosBuffer.Y+1 != len(dataBuffer) {
-				cursorPosBuffer.Y++
-				if len(dataBuffer[cursorPosBuffer.Y]) < cursorPosBuffer.X {
-					cursorPosBuffer.X = len(dataBuffer[cursorPosBuffer.Y])
-				}
-				syncCursor()
-			}
-		case tcell.KeyUp:
-			if cursorPosBuffer.Y != 0 {
-				cursorPosBuffer.Y--
-				if len(dataBuffer[cursorPosBuffer.Y]) < cursorPosBuffer.X {
-					cursorPosBuffer.X = len(dataBuffer[cursorPosBuffer.Y])
-				}
-				syncCursor()
-			}
-		default:
-			if escapePressed {
-				if e.Rune == 'q' {
-					Close()
-					os.Exit(0)
-				} else {
-					escapePressed = false
-				}
-			}
-			if e.Rune == 0 {
+		switch currentMode {
+		case MODE_INSERT:
+			if e.Value == tcell.KeyEscape {
+				currentMode = MODE_NORMAL
 				continue
 			}
-			line := dataBuffer[cursorPosBuffer.Y]
-			dataBuffer[cursorPosBuffer.Y] = line[:cursorPosBuffer.X] + string(e.Rune) + line[cursorPosBuffer.X:]
-			if e.Rune == 'x' {
+			handleKeyInsertMode(e)
+		case MODE_NORMAL:
+			switch e.Rune {
+			case ':':
+				currentMode = MODE_COMMAND_LINE
+			case 'i':
+				currentMode = MODE_INSERT
+			case 'q':
+				//TODO Remove this after the command line handler is implmented
 				Close()
-				fmt.Printf("Data: %v", dataBuffer)
+				os.Exit(0)
 			}
-			cursorPosBuffer.X++
-			syncTextFrame()
+		case MODE_COMMAND_LINE:
+			if e.Value == tcell.KeyEscape {
+				currentMode = MODE_NORMAL
+				// TODO reset any commands captured for command line
+				continue
+			}
+			// TODO capture the command string as typed
 		}
+
+	}
+}
+
+func handleKeyInsertMode(e actions.Event) {
+	switch e.Value {
+	case tcell.KeyEnter:
+		newBuffer := make([]string, len(dataBuffer)+1)
+		copy(newBuffer, dataBuffer)
+		if cursorPosBuffer.Y < len(dataBuffer)-1 {
+			for i := len(newBuffer) - 1; i > cursorPosBuffer.Y; i-- {
+				newBuffer[i] = newBuffer[i-1]
+			}
+		}
+
+		if cursorPosBuffer.X < len(dataBuffer[cursorPosBuffer.Y]) {
+			newBuffer[cursorPosBuffer.Y] = dataBuffer[cursorPosBuffer.Y][:cursorPosBuffer.X]
+			newBuffer[cursorPosBuffer.Y+1] = dataBuffer[cursorPosBuffer.Y][cursorPosBuffer.X:]
+		}
+		cursorPosBuffer.Y++
+		cursorPosBuffer.X = 0
+		dataBuffer = newBuffer
+		syncTextFrame()
+	case tcell.KeyLeft:
+		if cursorPosBuffer.X != 0 {
+			cursorPosBuffer.X--
+			syncCursor()
+		}
+	case tcell.KeyRight:
+		if cursorPosBuffer.X != len(dataBuffer[cursorPosBuffer.Y]) {
+			cursorPosBuffer.X++
+			syncCursor()
+		}
+	case tcell.KeyDown:
+		if cursorPosBuffer.Y+1 != len(dataBuffer) {
+			cursorPosBuffer.Y++
+			if len(dataBuffer[cursorPosBuffer.Y]) < cursorPosBuffer.X {
+				cursorPosBuffer.X = len(dataBuffer[cursorPosBuffer.Y])
+			}
+			syncCursor()
+		}
+	case tcell.KeyUp:
+		if cursorPosBuffer.Y != 0 {
+			cursorPosBuffer.Y--
+			if len(dataBuffer[cursorPosBuffer.Y]) < cursorPosBuffer.X {
+				cursorPosBuffer.X = len(dataBuffer[cursorPosBuffer.Y])
+			}
+			syncCursor()
+		}
+	default:
+		if e.Rune == 0 {
+			return
+		}
+		line := dataBuffer[cursorPosBuffer.Y]
+		dataBuffer[cursorPosBuffer.Y] = line[:cursorPosBuffer.X] + string(e.Rune) + line[cursorPosBuffer.X:]
+		if false && e.Rune == 'x' {
+			Close()
+			fmt.Printf("Data: %v", dataBuffer)
+		}
+		cursorPosBuffer.X++
+		syncTextFrame()
 	}
 }
 
